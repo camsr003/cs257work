c58cbf25e661222c74eb644ab3ee4d8a2c2d2993
camsr003 <165401262+camsr003@users.noreply.github.com>
2024-04-20 15:59:26 -0500

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -105,7 +105,7 @@ def smallest_MN():
 
 
     # Here the %s signals that we will replace this with a variable later
-    sql = "SELECT uscitypop, abb FROM state WHERE abb = %s"
+    sql = "SELECT uscitypop, abb FROM usstatepop WHERE abb = %s"
 
     state_abb1 = 'MN'
     
