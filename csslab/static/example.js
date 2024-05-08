4dd49d0bae5f3f148a8c89f3455a9566a0003007
camsr003 <165401262+camsr003@users.noreply.github.com>
2024-05-08 10:44:25 -0500

    Update example.js

--- csslab/static/example.js
+++ csslab/static/example.js
@@ -1,5 +1,5 @@
 function changeColor() {
-  the_heading = document.getElementById("hello");
+  the_heading = document.getElementById("nextpageText");
   the_heading.style.color = "red";
   console.log("I just changed the color to: " + the_heading.style.color)  
 }
