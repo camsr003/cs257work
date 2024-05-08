087599938204b747ef68d4d5f87cb8a055a0fe63
camsr003 <165401262+camsr003@users.noreply.github.com>
2024-05-08 10:29:44 -0500

    Update js-example.py

--- csslab/js-example.py
+++ csslab/js-example.py
@@ -8,7 +8,10 @@ def welcome():
     message = "Welcome to My Example Webpage."
     message = message + " This text was produced by concatenating strings in Python!"
     return render_template("homepage.html", someText = message)
-
+@app.route('/newpage')
+def nextpage():
+    message = "This is the next page."
+    return render_template("nextpage.html", someText = message)
 if __name__ == '__main__':
     my_port = 5124
     app.run(host='0.0.0.0', port = my_port) 
